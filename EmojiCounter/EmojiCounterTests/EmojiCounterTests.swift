67ea69fcfeac3717a963a643b519fbbfffa6f61a
Noah Flood <noahflood12@gmail.com>
2025-07-06 22:53:25 -0500

    setup

---
+++ EmojiCounter/EmojiCounterTests/EmojiCounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiCounterTests.swift
+//  EmojiCounterTests
+//
+//  Created by Noah Flood on 7/6/25.
+//
+
+import Testing
+@testable import EmojiCounter
+
+struct EmojiCounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
