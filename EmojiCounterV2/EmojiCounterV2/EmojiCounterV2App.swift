a469b8134420fee3a0c4b3723979d0332e8b17e7
Noah Flood <noahflood12@gmail.com>
2025-07-06 22:57:00 -0500

    restarted using all SwiftUI EmojiCounterV2

---
+++ EmojiCounterV2/EmojiCounterV2/EmojiCounterV2App.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiCounterV2App.swift
+//  EmojiCounterV2
+//
+//  Created by Noah Flood on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct EmojiCounterV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
